1fd532d7feb72509fc90d3e15853bf68ae0c4f3b
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-01 18:36:05 -0400

    added comments

--- work_or_sleep_in.py
+++ work_or_sleep_in.py
@@ -5,7 +5,7 @@ day = int(input('Enter a day as a number (0-6) and Ill tell you the day of the w
 prompt = 'Your chosen day is:'
 
 #determin the numerical value of the day from the user input
-if day == 5 or day == 0:
+if day == 6 or day == 0:
     print('Go back to bed')
 else:
     print('You better get up!!')
