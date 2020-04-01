f98bc8fdc449971c4f0f2112adf543a7c7fa8a87
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-01 15:23:36 -0400

    adding new file

---
+++ work_or_sleep_in.py
@@ -0,0 +1,11 @@
+#prompt user for a number 0-6 to be converted to a day of the week
+day = int(input('Enter a day as a number (0-6) and Ill tell you the day of the week '))
+
+#create a default prompt
+prompt = 'Your chosen day is:'
+
+#determin the numerical value of the day from the user input
+if day == 5 or day == 0:
+    print('Go back to bed')
+else:
+    print('You better get up!!')
