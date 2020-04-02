ee3051733a926bf4b465bb0b8b761de3c6b4dd3d
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-02 15:45:45 -0400

    added a function

--- work_or_sleep_in.py
+++ work_or_sleep_in.py
@@ -9,3 +9,20 @@ if day == 6 or day == 0:
     print('Go back to bed')
 else:
     print('You better get up!!')
+
+
+
+
+
+
+
+############solve with a function###########
+def sleep_or_work(day):
+    if day == 6 or day == 0:
+        return 'Go back to bed'
+    else:
+        return 'You better get up!!'
+
+day = int(input('Enter a day as a number (0-6) and Ill tell you the day of the week '))
+message = sleep_or_work(day)
+print(message)
